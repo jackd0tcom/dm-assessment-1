066528b28769fbe870f4fde934a8b0ac53c482b1
Jack Ball <ball.jack2376@gmail.com>
2023-08-04 11:00:56 -0600

    a couple bugs in js1 still

--- javascript-1/practice-js-1.js
+++ javascript-1/practice-js-1.js
@@ -1,17 +1,17 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
-//CODE HERE
+let myName = 'Jack'
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
-//CODE HERE
+let faveNum = 77
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
-//CODE HERE
+let lovesCode = true
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -19,7 +19,9 @@
   The function should return the sum of the two parameters.
 */
 
-//CODE HERE
+function sum(num1, num2) {
+  return num1 + num2;
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -29,7 +31,11 @@
   If 'data' is a falsy value, return a false boolean.
 */
 
-//CODE HERE
+function trueOrFalse(data) {
+  if (data === false) {
+    return false
+  } return true
+}
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -39,7 +45,11 @@
   If the number is even, return the string: 'the number is even'.
 */
 
-//CODE HERE
+function oddChecker(num) {
+  if (num % 2 === 0) {
+    return 'the number is even'
+  } else return 'the number is odd'
+}
 
 //////////////////PROBLEM 7////////////////////
 /*
@@ -48,19 +58,21 @@
   "NAMEPARAM loves LOVEPARAM" with the appropriate parameters in the string. e.g. "Joseph loves music"
 */
 
-//CODE HERE
+function iLove(name, love) {
+  return `${name} loves ${love}`
+}
 
 //////////////////PROBLEM 8////////////////////
 /* Create a copy of the faveColors array called 'colorCopy' using the slice method. */
 
 const faveColors = ['red', 'green', 'black'];
 
-//CODE HERE
+let colorCopy = faveColors.slice();
 
 //////////////////PROBLEM 9////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
-//CODE HERE
+colorCopy.push('blue')
 
 //////////////////PROBLEM 10////////////////////
 /*
@@ -70,7 +82,7 @@ const faveColors = ['red', 'green', 'black'];
 
 const numbers = [1, 2, 3, 4, 5];
 
-//CODE HERE
+let middleNums = numbers.slice(1, 4)
 
 //////////////////PROBLEM 11////////////////////
 /*
@@ -86,7 +98,12 @@ const numbers = [1, 2, 3, 4, 5];
   For example: 'Hello! My name is Joseph and I live in Utah"
 */
 
-//CODE HERE
+let me = {
+  firstName: 'Jack',
+  state: 'Utah',
+  age: 22,
+  greeter: function () { `Hello! My name is ${firstName} and I live in ${state}` }
+}
 
 //////////////////PROBLEM 12////////////////////
 /* 
@@ -101,7 +118,19 @@ const numbers = [1, 2, 3, 4, 5];
   Return the answers array inside of the function.
 */
 
-// CODE HERE
+
+function bigOrSmall(arr) {
+  let answers = [];
+  for (i of arr) {
+    if (arr[i] > 100) {
+      answers.push('big')
+    } else {
+      if (arr[i] <= 100) {
+        answers.push('small')
+      }
+    }
+  } console.log(answers)
+}
 
 //////////////////PROBLEM 13////////////////////
 /* 
@@ -113,7 +142,12 @@ const numbers = [1, 2, 3, 4, 5];
   Finally, return the 'reversed' array variable.
 */
 
-//CODE HERE
+function arrayReverser(arr) {
+  let reversed = []
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reversed.push(arr[i])
+  } return reversed
+}
 
 //////////////////PROBLEM 14////////////////////
 
@@ -139,47 +173,58 @@ function secondFunction() {
 */
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = ['global', 'inner', 'outer', 'functional'];
+let globalScope = ['global'];
 
 //This array should contain the variable names (as strings) accessible in the firstFunction function.
-let firstFunctionScope = ['global', 'inner', 'outer', 'functional'];
+let firstFunctionScope = ['global', 'outer'];
 
 //This array should contain the variable names (as strings) accessible in the innerFunction function.
-let innerFunctionScope = ['global', 'inner', 'outer', 'functional'];
+let innerFunctionScope = ['global', 'inner', 'outer'];
 
 //This array should contain the variable names (as strings) accessible in the secondFunction function.
-let secondFunctionScope = ['global', 'inner', 'outer', 'functional'];
+let secondFunctionScope = ['global', 'functional'];
 
 //////////////////PROBLEM 15////////////////////
-/* 
-  Write a function called firstItem that takes in two parameters: an array and a callback.  
+/*
+  Write a function called firstItem that takes in two parameters: an array and a callback.
   Invoke the callback passing the first item in the array as an argument.
 */
 
-//CODE HERE
+function firstItem(arr, cb) {
+  cb(arr[0])
+}
 
 //////////////////PROBLEM 16////////////////////
-/* 
-  Write a function called isItBob that takes in two arguments: an object and a callback function. 
-  Check if the object's name property is equal to 'Bob'.  
-  If it is, invoke the callback passing in true as an argument.  
+/*
+  Write a function called isItBob that takes in two arguments: an object and a callback function.
+  Check if the object's name property is equal to 'Bob'.
+  If it is, invoke the callback passing in true as an argument.
   Otherwise, invoke the callback passing in false.
 */
 
-//CODE HERE
+function isItBob(obj, cb) {
+  if (obj.name === 'Bob') {
+    cb(true)
+  } else cb(false)
+}
 
 //////////////////PROBLEM 17////////////////////
 /*
-  Write a function called giveMeDoubles that takes in two arguments: an array of numbers and a callback.  
+  Write a function called giveMeDoubles that takes in two arguments: an array of numbers and a callback.
   Use a for loop to double all numbers in the array.
   Then invoke the callback, passing in the doubled array.
 */
 
-//CODE HERE
+function giveMeDoubles(arr, cb) {
+  let doubled = []
+  for (let i = 0; i <= arr.length - 1; i++) {
+    doubled.push(arr[i] * 2);
+  } cb(doubled)
+}
 
 //////////////////PROBLEM 18////////////////////
 /*
-  Write a function called carFactory that takes in three parameters: a make, model, and year.  
+  Write a function called carFactory that takes in three parameters: a make, model, and year.
   When the function is invoked:
     - a string will be sent in for make
     - a string will be sent in for model
@@ -195,11 +240,22 @@ let secondFunctionScope = ['global', 'inner', 'outer', 'functional'];
     carFactory('toyota', 'camry', 2020)
   should return an object that looks like this:
   {
-    make: 'toyota', 
+    make: 'toyota',
     model: 'camry',
     year: 2020,
     isNew: true
   }
 */
 
-//CODE HERE
+
+function carFactory(make, model, year) {
+  obj = {
+    make: make,
+    model: model,
+    year: year
+  }
+  if (year > 2018) {
+    obj.isNew = true
+  } else { obj.isNew = false }
+  return obj
+}
--- javascript-2/practice-js-2.js
+++ javascript-2/practice-js-2.js
@@ -9,7 +9,7 @@
     It should be a string containing your best definition of what Git is.
 */
 
-//CODE HERE
+let gitDefinition = 'Git is a way to back up your code off your computer.'
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -17,47 +17,59 @@
     It should be a string containing your best definition of what GitHub is.
 */
 
-//CODE HERE
+let gitHubDefinition = 'gitHub is a platform used to store code files, and share them with others.'
 
 //////////////////PROBLEMS 3 - 9////////////////////
 /*
-    For the next several problems you will be creating objects containing information about different git 
-    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property 
-    will be a string with a description of what that git command does.  The 'code' property should be a 
+    For the next several problems you will be creating objects containing information about different git
+    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property
+    will be a string with a description of what that git command does.  The 'code' property should be a
     string of the actual command (what you type into your terminal).
 */
 
 //////////////////PROBLEM 3////////////////////
 /*
-    Create an object called 'init' with 'description' and 'code' properties 
+    Create an object called 'init' with 'description' and 'code' properties
     following the guidelines above to describe the init command.
 */
 
-//CODE HERE
+let init = {
+    description: 'the command used to create a local repository',
+    code: 'git init'
+}
 
 //////////////////PROBLEM 4////////////////////
 /*
-    Create an object called 'clone' with 'description' and 'code' properties 
+    Create an object called 'clone' with 'description' and 'code' properties
     following the guidelines above to describe the clone command.
 */
 
-//CODE HERE
+let clone = {
+    description: 'the command used to make a copy of a forked repository',
+    code: 'git clone'
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
-    Create an object called 'status' with 'description' and 'code' properties 
+    Create an object called 'status' with 'description' and 'code' properties
     following the guidelines above to describe the status command.
 */
 
-//CODE HERE
+let status = {
+    description: 'the command used to view the status of the files in the repository',
+    code: 'git status'
+}
 
 //////////////////PROBLEM 6////////////////////
 /*
-    Create an object called 'add' with 'description' and 'code' properties 
+    Create an object called 'add' with 'description' and 'code' properties
     following the guidelines above to describe the add command.
 */
 
-//CODE HERE
+let add = {
+    description: 'the command used to add files to the repository',
+    code: 'git add'
+}
 
 //////////////////PROBLEM 7////////////////////
 /*
@@ -65,7 +77,10 @@
     following the guidelines above to describe the commit command.
 */
 
-//CODE HERE
+let commit = {
+    description: 'the command used to save or commit files to the repository',
+    code: 'git commit -m "descriptive commit message"'
+}
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,12 +88,18 @@
     following the guidelines above to describe the command to add a remote location to your git repository.
 */
 
-//CODE HERE
+let addRemote = {
+    description: 'the command used to link your local repository with the repository created on gitHub',
+    code: 'git remote add origin'
+}
 
 //////////////////PROBLEM 9////////////////////
 /*
-    Create an object called 'push' with 'description' and 'code' properties 
+    Create an object called 'push' with 'description' and 'code' properties
     following the guidelines above to describe the push command.
 */
 
-//CODE HERE
+let push = {
+    description: 'the command used to upload the commits to gitHub',
+    code: 'git push'
+}
